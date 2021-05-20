9be387aba5b3d35153e04e69fcafceb9476dca58
byamron <ben.yamron@icloud.com>
2021-05-19 23:10:12 -0400

    lots of changes

--- Final.R
+++ Final.R
@@ -1,9 +1,4 @@
-#this is a test commit
-#hi ben
-#hi ben
-#hi ben
 
-#whatsup
 
 library(tidyverse)
 library(rvest)
@@ -16,6 +11,7 @@ library(reshape2)
 library(lubridate)
 library(geojsonio)
 
+#read in data
 noncampus.crime <- read_csv("noncampuscrime161718.csv")
 noncampus.vawa <- read_csv("noncampusvawa161718.csv")
 oncampus.crime <- read_csv("oncampuscrime161718.csv")
@@ -134,6 +130,7 @@ vawa.joined <- vawa.list %>% reduce(inner_join, by = c("UNITID_P",
                                                        "women_total",
                                                        "Total"))
 
+#join all data sets
 total.joined <- crime.joined %>%
   inner_join(vawa.joined, by = c("UNITID_P",
                                 "INSTNM",
@@ -147,33 +144,43 @@ total.joined <- crime.joined %>%
                                 "men_total",
                                 "women_total",
                                 "Total")) %>%
-  filter(Sector_desc %in% c("Public, 4-year or above", "Private nonprofit, 4-year or above"))
+  filter(Sector_desc %in% c("Public, 4-year or above", "Private nonprofit, 4-year or above")) %>%
+  filter(!(is.na(State)))
 
+#rename columns based on original data sets
+colnames(total.joined) <- str_replace_all(colnames(total.joined),"x", "OFF")
+colnames(total.joined) <- str_replace_all(colnames(total.joined),"y", "ON")
 
+#create data frame where NAs are replaced with zeros
+total.joined.replaced.na <- total.joined %>%
+  filter(!(is.na(State)))
+total.joined.replaced.na[is.na(total.joined.replaced.na)] = 0
 
-#this is the code for state and public colleges in the US
-state.colleges.fixed <- read.csv("colleges.edit.csv") 
-state.colleges.fixed2 <- as.data.frame(str_replace_all(string = state.colleges.fixed$div.location," \\s*\\([^\\)]+\\)", ""))
-
-names(state.colleges.fixed2)[1] <- "INSTNM"
-
-state.schools.total.joined <- total.joined %>%
-  inner_join(state.colleges.fixed2, by = "INSTNM") %>%
-
-
-state.schools.total.not.joined <- state.colleges.fixed2 %>%
-  anti_join(total.joined, by = "INSTNM")
+total.joined.replaced.na %>%
+  group_by(INSTNM) %>%
+  summarize(total.rape = RAPE16 + RAPE16.OFF + RAPE16.ON + 
+              RAPE17 + RAPE17.OFF + RAPE17.ON +
+              RAPE18 + RAPE18.OFF + RAPE18.ON,
+            total.statr = STATR16 + STATR16.OFF + STATR16.ON + 
+              STATR17 + STATR17.OFF + STATR17.ON +
+              STATR18 + STATR18.OFF + STATR18.ON,
+            total.fondl = FONDL16 + FONDL16.OFF + FONDL16.ON + 
+              FONDL17 + FONDL17.OFF + FONDL17.ON +
+              FONDL18 + FONDL18.OFF + FONDL18.ON,
+            total.vawa = DOMEST16 + DOMEST16.OFF + DOMEST16.ON +
+              DOMEST17 + DOMEST17.OFF + DOMEST17.ON +
+              DOMEST18 + DOMEST18.OFF + DOMEST18.ON +
+              DATING16 + DATING16.OFF + DATING16.ON +
+              DATING17 + DATING17.OFF + DATING17.ON +
+              DATING18 + DATING18.OFF + DATING18.ON +
+              STALK16 + STALK16.OFF + STALK16.ON +
+              STALK17 + STALK17.OFF + STALK17.ON +
+              STALK18 + STALK18.OFF + STALK18.ON) %>%
+  view()
 
 #read in geojson file
 college.shapes <- geojson_read("Colleges_and_Universities.geojson",
              what = "sp")
 
 
-## read in usnews data
-usnews.url <- "https://www.usnews.com/best-colleges/rankings/national-universities/top-public"
-
-usnews.url %>%
-  read_html() %>%
-  html_nodes("h3") %>%
-  html_text()
 
